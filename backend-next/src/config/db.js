c097de577e0537033888e4df43dd730a7f959f0a
Mariono Silaban <silabanmariono91@gmail.com>
2026-03-13 09:24:47 +0700

    change content

--- backend-next/src/config/db.js
+++ backend-next/src/config/db.js
@@ -1,7 +1,19 @@
 import { PrismaClient } from "@prisma/client"
+import { Pool } from "pg"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
 
 const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
 })
 
 const connectDB = async () => {
@@ -9,7 +21,7 @@ const connectDB = async () => {
     await prisma.$connect()
     console.log("DB Terhubung via Prisma")
   } catch (error) {
-    console.error("Database gagal terhubung:", error.message)
+    console.log("Database gagal terhubung:", error.message)
     process.exit(1)
   }
 }
